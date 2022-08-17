da4846d1ee974adc48799ef1662835160f3a6e2e
Picool <71496218+VickuTT@users.noreply.github.com>
2022-08-17 15:51:40 +0800

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,30 @@
 # report.py
 #
 # Exercise 2.4
+#直接读入和打开
+import csv
+
+portfolio = []
+with open((
+          'C:/Users/cony19980509/Desktop/动手学数据分析/practical-python-master/practical-python-master/Work/Data/portfolio.csv'),
+          'rt') as f:
+    rows = csv.reader(f)
+    next(rows)
+    for row in rows:
+        holding = (row[0], int(row[1]), float(row[2]))
+        portfolio.append(holding)
+#通过设置外部函数和调用的形式
+import csv
+
+def read_portfolio(filename):
+    portfolio=[]
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
+
+port=read_portfolio('C:/Users/cony19980509/Desktop/动手学数据分析/practical-python-master/practical-python-master/Work/Data/portfolio.csv')
+
